6187f4fe84e9614d0fc10f6cc9db6eb62408ec8e
aryantorfehneja <aryan.torfeh-nejad@ocsbstudent.ca>
2021-03-31 19:08:37 +0000

    fixed

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-document.getElementById('button').addEventListener('click',click)
+document.getElementById('button').addEventListener('click', click)
 
-let firstNum=0
-let secondNum=0
-let counter=0
+let firstNum = 0
+let secondNum = 0
+let counter = 0
 let answer = 0
-let negative=0
+let negative = 0
 
 function click () {
-  firstNum = document.getElementById ('input').value
-  secondNum = document.getElementById ('input2').value
+  firstNum = document.getElementById('input').value
+  secondNum = document.getElementById('input2').value
   firstNum = parseInt(firstNum)
   secondNum = parseInt(secondNum)
   answer = 0
@@ -23,9 +23,8 @@ function click () {
     secondNum = Math.abs(secondNum)
   }
 
-  for (counter = 0; counter < firstNum; counter ++)
-  {
-    answer = answer + secondNum
+  for (counter = 0; counter < firstNum; counter++)
+  { answer = answer + secondNum
   }
   document.getElementById('answer').innerHTML = answer
 }
